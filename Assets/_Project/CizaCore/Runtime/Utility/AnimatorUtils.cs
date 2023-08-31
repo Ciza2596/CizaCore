d48871a8c7f78942da5a0b84555974da8813e5ea
Ciza2596 <Ciza2596@gmail.com>
2023-09-01 00:02:01 +0800

    Update

--- Assets/_Project/CizaCore/Runtime/Utility/AnimatorUtils.cs
+++ Assets/_Project/CizaCore/Runtime/Utility/AnimatorUtils.cs
@@ -4,133 +4,131 @@ using UnityEngine;
 
 namespace CizaCore
 {
-    public static class AnimatorUtils
-    {
-        public const string SpeedRateParameterName = "SpeedRate";
-        public static readonly int SpeedRateParameterHash = Animator.StringToHash(SpeedRateParameterName);
+	public static class AnimatorUtils
+	{
+		public const           string SpeedRateParameterName = "SpeedRate";
+		public static readonly int    SpeedRateParameterHash = Animator.StringToHash(SpeedRateParameterName);
 
-        public static void PlayAtStart(this Animator animator, int stateNameHash, int layerIndex = 0) =>
-            animator.Play(stateNameHash, layerIndex, 0);
-        
+		public static void PlayAtStart(this Animator animator, int stateNameHash, int layerIndex = 0)
+		{
+			animator.Play(stateNameHash, layerIndex, 0);
+			animator.Update(0);
+		}
 
-        public static async UniTask PlayAtStart(this Animator animator, int stateNameHash, float endNormalizedTime, int layerIndex = 0, CancellationToken cancellationToken = default)
-        {
-            animator.PlayAtStart(stateNameHash, layerIndex);
-            await animator.WaitAnimCompletedByStateNameHash(stateNameHash, endNormalizedTime, cancellationToken);
-            animator.SetSpeedRate(0);
-        }
+		public static async UniTask PlayAtStart(this Animator animator, int stateNameHash, float endNormalizedTime = 1, int layerIndex = 0, CancellationToken cancellationToken = default)
+		{
+			animator.PlayAtStart(stateNameHash, layerIndex);
+			await animator.WaitAnimCompletedByStateNameHash(stateNameHash, endNormalizedTime, cancellationToken);
+			animator.SetSpeedRate(0);
+		}
 
-        public static async UniTask Play(this Animator animator, int stateNameHash, float endNormalizedTime, int layerIndex = 0, CancellationToken cancellationToken = default)
-        {
-            animator.Play(stateNameHash, layerIndex);
-            await animator.WaitAnimCompletedByStateNameHash(stateNameHash, endNormalizedTime, cancellationToken);
-            animator.SetSpeedRate(0);
-        }
+		public static async UniTask Play(this Animator animator, int stateNameHash, float endNormalizedTime = 1, int layerIndex = 0, CancellationToken cancellationToken = default)
+		{
+			animator.Play(stateNameHash, layerIndex);
+			await animator.WaitAnimCompletedByStateNameHash(stateNameHash, endNormalizedTime, cancellationToken);
+			animator.SetSpeedRate(0);
+		}
 
-        public static void Play(this Animator animator, int stateNameHash, float time, ref float duration, int layerIndex = 0)
-        {
-            var normalizedTime = TimeUtils.GetNormalizedTime(time, ref duration);
-            animator.Play(stateNameHash, layerIndex, normalizedTime);
-        }
+		public static void Play(this Animator animator, int stateNameHash, float time, ref float duration, int layerIndex = 0)
+		{
+			var normalizedTime = TimeUtils.GetNormalizedTime(time, ref duration);
+			animator.Play(stateNameHash, layerIndex, normalizedTime);
+		}
 
+		public static async UniTask WaitAnimCompletedByStateNameHash(this Animator animator, int stateNameHash, float endNormalizedTime = 1, CancellationToken cancellationToken = default)
+		{
+			await animator.WaitChangeStateByStateNameHash(stateNameHash, cancellationToken);
+			await animator.WaitAnimCompleted(endNormalizedTime, cancellationToken);
+		}
 
-        public static async UniTask WaitAnimCompletedByStateNameHash(this Animator animator, int stateNameHash, float endNormalizedTime = 1, CancellationToken cancellationToken = default)
-        {
-            await animator.WaitChangeStateByStateNameHash(stateNameHash, cancellationToken);
-            await animator.WaitAnimCompleted(endNormalizedTime, cancellationToken);
-        }
+		public static async UniTask WaitAnimCompletedByTagHash(this Animator animator, int tagHash, float endNormalizedTime = 1, CancellationToken cancellationToken = default)
+		{
+			await animator.WaitChangeStateByTagHash(tagHash, cancellationToken);
+			await animator.WaitAnimCompleted(endNormalizedTime, cancellationToken);
+		}
 
+		public static async UniTask WaitChangeStateByStateNameHash(this Animator animator, int stateNameHash, CancellationToken cancellationToken)
+		{
+			while (animator.GetCurrentStateNameHash() != stateNameHash)
+				await UniTask.Yield(PlayerLoopTiming.LastPostLateUpdate, cancellationToken);
+		}
 
-        public static async UniTask WaitAnimCompletedByTagHash(this Animator animator, int tagHash, float endNormalizedTime = 1, CancellationToken cancellationToken = default)
-        {
-            await animator.WaitChangeStateByTagHash(tagHash, cancellationToken);
-            await animator.WaitAnimCompleted(endNormalizedTime, cancellationToken);
-        }
+		public static async UniTask WaitChangeStateByTagHash(this Animator animator, int tagHash, CancellationToken cancellationToken)
+		{
+			while (animator.GetCurrentTagHash() != tagHash)
+				await UniTask.Yield(PlayerLoopTiming.LastPostLateUpdate, cancellationToken);
+		}
 
+		public static async UniTask WaitAnimCompleted(this Animator animator, float endNormalizedTime = 1, CancellationToken cancellationToken = default)
+		{
+			TimeUtils.CheckNormalizedTime(ref endNormalizedTime);
+			while (animator.GetCurrentNormalizedTime() < endNormalizedTime)
+				await UniTask.Yield(PlayerLoopTiming.LastPostLateUpdate, cancellationToken);
+		}
 
-        public static async UniTask WaitChangeStateByStateNameHash(this Animator animator, int stateNameHash, CancellationToken cancellationToken)
-        {
-            while (animator.GetCurrentStateNameHash() != stateNameHash)
-                await UniTask.Yield(PlayerLoopTiming.LastPostLateUpdate, cancellationToken);
-        }
+		public static Animator AdjustSpeedRateByDuration(this Animator animator, ref float duration, int layerIndex = 0)
+		{
+			var speedRate = animator.GetSpeedRate(ref duration, layerIndex);
+			animator.SetSpeedRate(speedRate);
+			return animator;
+		}
 
-        public static async UniTask WaitChangeStateByTagHash(this Animator animator, int tagHash, CancellationToken cancellationToken)
-        {
-            while (animator.GetCurrentTagHash() != tagHash)
-                await UniTask.Yield(PlayerLoopTiming.LastPostLateUpdate, cancellationToken);
-        }
+		public static Animator SetSpeedRate(this Animator animator, float speedRate = 1)
+		{
+			animator.SetFloat(SpeedRateParameterHash, speedRate);
+			return animator;
+		}
 
+		public static int GetCurrentStateNameHash(this Animator animator, int layerIndex = 0)
+		{
+			var currentAnimatorStateInfo = animator.GetCurrentAnimatorStateInfo(layerIndex);
+			return currentAnimatorStateInfo.shortNameHash;
+		}
 
-        public static async UniTask WaitAnimCompleted(this Animator animator, float endNormalizedTime = 1, CancellationToken cancellationToken = default)
-        {
-            TimeUtils.CheckNormalizedTime(ref endNormalizedTime);
-            while (animator.GetCurrentNormalizedTime() < endNormalizedTime)
-                await UniTask.Yield(PlayerLoopTiming.LastPostLateUpdate, cancellationToken);
-        }
+		public static int GetCurrentTagHash(this Animator animator, int layerIndex = 0)
+		{
+			var currentAnimatorStateInfo = animator.GetCurrentAnimatorStateInfo(layerIndex);
+			return currentAnimatorStateInfo.tagHash;
+		}
 
-        public static Animator AdjustSpeedRateByDuration(this Animator animator, ref float duration, int layerIndex = 0)
-        {
-            var speedRate = animator.GetSpeedRate(ref duration, layerIndex);
-            animator.SetSpeedRate(speedRate);
-            return animator;
-        }
+		public static float GetCurrentNormalizedTime(this Animator animator, int layerIndex = 0)
+		{
+			var currentAnimatorStateInfo = animator.GetCurrentAnimatorStateInfo(layerIndex);
+			return currentAnimatorStateInfo.normalizedTime;
+		}
 
-        public static Animator SetSpeedRate(this Animator animator, float speedRate = 1)
-        {
-            animator.SetFloat(SpeedRateParameterHash, speedRate);
-            return animator;
-        }
+		public static float GetCurrentClipLength(this Animator animator, int layerIndex = 0)
+		{
+			var clip = animator.GetCurrentClip(0);
+			return clip.length;
+		}
 
-        public static int GetCurrentStateNameHash(this Animator animator, int layerIndex = 0)
-        {
-            var currentAnimatorStateInfo = animator.GetCurrentAnimatorStateInfo(layerIndex);
-            return currentAnimatorStateInfo.shortNameHash;
-        }
+		public static AnimationClip GetCurrentClip(this Animator animator, int layerIndex = 0)
+		{
+			var currentAnimatorClipInfos = animator.GetCurrentAnimatorClipInfo(layerIndex);
+			var currentAnimatorClipInfo  = currentAnimatorClipInfos[0];
+			var clip                     = currentAnimatorClipInfo.clip;
+			return clip;
+		}
 
-        public static int GetCurrentTagHash(this Animator animator, int layerIndex = 0)
-        {
-            var currentAnimatorStateInfo = animator.GetCurrentAnimatorStateInfo(layerIndex);
-            return currentAnimatorStateInfo.tagHash;
-        }
+		public static float GetSpeedRate(this Animator animator, ref float duration, int layerIndex = 0)
+		{
+			TimeUtils.CheckDuration(ref duration);
+			var clipLength = animator.GetCurrentClipLength(layerIndex);
+			return clipLength / duration;
+		}
 
-        public static float GetCurrentNormalizedTime(this Animator animator, int layerIndex = 0)
-        {
-            var currentAnimatorStateInfo = animator.GetCurrentAnimatorStateInfo(layerIndex);
-            return currentAnimatorStateInfo.normalizedTime;
-        }
+		public static bool CheckHasSpeedRateParameter(this Animator animator) =>
+			animator.CheckHasParameter(SpeedRateParameterName);
 
-        public static float GetCurrentClipLength(this Animator animator, int layerIndex = 0)
-        {
-            var clip = animator.GetCurrentClip(0);
-            return clip.length;
-        }
-        
-        public static AnimationClip GetCurrentClip(this Animator animator, int layerIndex = 0)
-        {
-            var currentAnimatorClipInfos = animator.GetCurrentAnimatorClipInfo(layerIndex);
-            var currentAnimatorClipInfo = currentAnimatorClipInfos[0];
-            var clip = currentAnimatorClipInfo.clip;
-            return clip;
-        } 
+		public static bool CheckHasParameter(this Animator animator, string parameterName)
+		{
+			var parameters = animator.parameters;
+			foreach (var parameter in parameters)
+				if (parameter.name == parameterName)
+					return true;
 
-        public static float GetSpeedRate(this Animator animator, ref float duration, int layerIndex = 0)
-        {
-            TimeUtils.CheckDuration(ref duration);
-            var clipLength = animator.GetCurrentClipLength(layerIndex);
-            return clipLength / duration;
-        }
-
-        public static bool CheckHasSpeedRateParameter(this Animator animator) =>
-            animator.CheckHasParameter(SpeedRateParameterName);
-
-        public static bool CheckHasParameter(this Animator animator, string parameterName)
-        {
-            var parameters = animator.parameters;
-            foreach (var parameter in parameters)
-                if (parameter.name == parameterName)
-                    return true;
-
-            return false;
-        }
-    }
+			return false;
+		}
+	}
 }
