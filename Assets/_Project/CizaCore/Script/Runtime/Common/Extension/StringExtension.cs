59fbac88b180f467f6d09776f1dcde55c806d606
Ciza2596 <Ciza2596@gmail.com>
2023-09-06 23:09:56 +0800

    Update 0.0.5 version

--- Assets/_Project/CizaCore/Script/Runtime/Common/Extension/StringExtension.cs
+++ Assets/_Project/CizaCore/Script/Runtime/Common/Extension/StringExtension.cs
@@ -9,22 +9,31 @@ namespace CizaCore
 		public static bool HasValue(this string str) =>
 			!string.IsNullOrEmpty(str) && !string.IsNullOrWhiteSpace(str);
 
+		public static string WithoutSpace(this string str) =>
+			str.Replace(" ", "");
+
 		public static string[] ToArray(this string str) =>
 			str.ToList().ToArray();
 
 		public static List<string> ToList(this string str)
 		{
 			var list = new List<string>();
-			if (!str.HasValue() || !str.Contains(SplitTag))
+			if (!str.HasValue())
 				return list;
 
-			var splitStrs = str.Split(SplitTag);
+			var strWithoutSpace = str.WithoutSpace();
+			if (!strWithoutSpace.Contains(SplitTag))
+			{
+				list.Add(strWithoutSpace);
+				return list;
+			}
+
+			var splitStrs = strWithoutSpace.Split(SplitTag);
 			foreach (var splitStr in splitStrs)
 			{
 				if (!splitStr.HasValue())
 					continue;
 
-				var strWithoutSpace = splitStr.Replace(" ", "");
 				list.Add(strWithoutSpace);
 			}
 
