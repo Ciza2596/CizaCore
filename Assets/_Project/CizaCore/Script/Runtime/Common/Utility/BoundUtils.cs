3f4de52ea6c7ad57d247ed10c5b39fea3f743f57
Ciza2596 <Ciza2596@gmail.com>
2023-09-08 00:04:18 +0800

    version 0.0.10

--- Assets/_Project/CizaCore/Script/Runtime/Common/Utility/BoundUtils.cs
+++ Assets/_Project/CizaCore/Script/Runtime/Common/Utility/BoundUtils.cs
@@ -8,24 +8,49 @@ namespace CizaCore
 		{
 			var bounds = new Bounds(boundPosition, boundSize);
 
-			var halfWidth  = addBoundSize.x / 2;
-			var halfHeight = addBoundSize.y / 2;
-
-			var topLeft = new Vector2(addBoundPosition.x - halfWidth, addBoundPosition.x + halfHeight);
+			var topLeft = GetTopLeft(addBoundPosition, addBoundSize);
 			bounds.Expand(topLeft);
 
-			var topRight = new Vector2(addBoundPosition.x + halfWidth, addBoundPosition.x + halfHeight);
+			var topRight = GetTopRight(addBoundPosition, addBoundSize);
 			bounds.Expand(topRight);
 
-			var bottomLeft = new Vector2(addBoundPosition.x - halfWidth, addBoundPosition.x - halfHeight);
+			var bottomLeft = GetBottomLeft(addBoundPosition, addBoundSize);
 			bounds.Expand(bottomLeft);
 
-			var bottomRight = new Vector2(addBoundPosition.x + halfWidth, addBoundPosition.x - halfHeight);
+			var bottomRight = GetBottomRight(addBoundPosition, addBoundSize);
 			bounds.Expand(bottomRight);
 
 			var center  = bounds.center;
 			var extents = bounds.extents;
 			return (new Vector2(center.x, center.y), new Vector2(extents.x, extents.y));
+		}
+
+		public static Vector2 GetTopLeft(Vector2 position, Vector2 size)
+		{
+			var halfWidth  = size.x / 2;
+			var halfHeight = size.y / 2;
+			return new Vector2(position.x - halfWidth, position.x + halfHeight);
+		}
+
+		public static Vector2 GetTopRight(Vector2 position, Vector2 size)
+		{
+			var halfWidth  = size.x / 2;
+			var halfHeight = size.y / 2;
+			return new Vector2(position.x + halfWidth, position.x + halfHeight);
+		}
+
+		public static Vector2 GetBottomLeft(Vector2 position, Vector2 size)
+		{
+			var halfWidth  = size.x / 2;
+			var halfHeight = size.y / 2;
+			return new Vector2(position.x - halfWidth, position.x - halfHeight);
+		}
+
+		public static Vector2 GetBottomRight(Vector2 position, Vector2 size)
+		{
+			var halfWidth  = size.x / 2;
+			var halfHeight = size.y / 2;
+			return new Vector2(position.x + halfWidth, position.x - halfHeight);
 		}
 	}
 }
