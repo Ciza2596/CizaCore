7af6aa8df98909ec84f58bbba6efb6dc2a810b7e
Ciza2596 <Ciza2596@gmail.com>
2023-11-25 12:09:50 +0800

    Update 0.0.54

---
+++ Assets/_Project/CizaCore/Script/Runtime/Anim/AnimSettings.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace CizaCore
+{
+	[Serializable]
+	public class AnimSettings
+	{
+		[SerializeField]
+		private string _showStateName = "Show";
+
+		[SerializeField]
+		private float _showEndNormalizedTime = 0.95f;
+
+		[Space]
+		[SerializeField]
+		private string _hideStateName = "Hide";
+
+		[SerializeField]
+		private float _hideEndNormalizedTime = 0.95f;
+
+		[Space]
+		[SerializeField]
+		private Animator _animator;
+
+		public void SetIsActive(bool isActive) =>
+			_animator.gameObject.SetActive(isActive);
+
+		public void Refresh() =>
+			_animator.Refresh();
+
+		public UniTask PlayShowAsync(CancellationToken cancellationToken) =>
+			PlayShowAsync(0, _showEndNormalizedTime, cancellationToken);
+
+		public async void PlayShowComplete() =>
+			await PlayShowAsync(1, 1, default);
+
+		public UniTask PlayHideAsync(CancellationToken cancellationToken) =>
+			_animator.PlayAtStartAsync(Animator.StringToHash(_hideStateName), endNormalizedTime: _hideEndNormalizedTime, cancellationToken: cancellationToken);
+
+		private UniTask PlayShowAsync(float startNormalizedTime, float endNormalizedTime, CancellationToken cancellationToken) =>
+			_animator.PlayAsync(Animator.StringToHash(_showStateName), startNormalizedTime: startNormalizedTime, endNormalizedTime: endNormalizedTime, cancellationToken: cancellationToken);
+	}
+}
