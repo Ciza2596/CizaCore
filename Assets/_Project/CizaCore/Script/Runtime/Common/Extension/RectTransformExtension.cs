2920cd1754ab9f853c08f18130ca872cffb7833a
Ciza2596 <Ciza2596@gmail.com>
2023-12-20 01:13:18 +0800

    Update 0.0.64

--- Assets/_Project/CizaCore/Script/Runtime/Common/Extension/RectTransformExtension.cs
+++ Assets/_Project/CizaCore/Script/Runtime/Common/Extension/RectTransformExtension.cs
@@ -2,56 +2,70 @@ using UnityEngine;
 
 namespace CizaCore
 {
-	public static class RectTransformExtension
-	{
-		public static Vector2 GetScreenPoint(this Transform transform, Camera camera = null)
-		{
-			var uiCamera    = camera is null ? Camera.main : camera;
-			var screenPoint = uiCamera.WorldToScreenPoint(transform.position);
-			return screenPoint;
-		}
+    public static class RectTransformExtension
+    {
+        public const float PivotCenter = 0.5f;
 
-		public static Vector2 GetScreenPoint(this RectTransform rectTransform, Camera camera = null)
-		{
-			var uiCamera    = camera is null ? Camera.main : camera;
-			var screenPoint = RectTransformUtility.WorldToScreenPoint(uiCamera, rectTransform.transform.position);
-			return screenPoint;
-		}
+        public static Vector2 GetScreenPoint(this Transform transform, Camera camera = null)
+        {
+            var uiCamera = camera is null ? Camera.main : camera;
+            var screenPoint = uiCamera.WorldToScreenPoint(transform.position);
+            return screenPoint;
+        }
 
-		public static float GetLeftWidth(this RectTransform rectTransform)
-		{
-			var width      = rectTransform.rect.width;
-			var leftPivotX = rectTransform.pivot.x;
+        public static Vector2 GetCenterPosition(this RectTransform rectTransform)
+        {
+            var rectTransformPosition = rectTransform.position;
+            var rectTransformRect = rectTransform.rect;
 
-			var leftWidth = width * leftPivotX;
-			return leftWidth;
-		}
+            var pivot = rectTransform.pivot;
+            var diffPivotX = PivotCenter - pivot.x;
+            var diffPivotY = PivotCenter - pivot.y;
 
-		public static float GetRightWidth(this RectTransform rectTransform)
-		{
-			var width       = rectTransform.rect.width;
-			var rightPivotX = (1 - rectTransform.pivot.x);
+            return new Vector2(rectTransformPosition.x + diffPivotX * rectTransformRect.width, rectTransformPosition.y + diffPivotY * rectTransformRect.height);
+        }
 
-			var rightWidth = width * rightPivotX;
-			return rightWidth;
-		}
+        public static Vector2 GetScreenPoint(this RectTransform rectTransform, Camera camera = null)
+        {
+            var uiCamera = camera is null ? Camera.main : camera;
+            var screenPoint = RectTransformUtility.WorldToScreenPoint(uiCamera, rectTransform.transform.position);
+            return screenPoint;
+        }
 
-		public static float GetUpperHeight(this RectTransform rectTransform)
-		{
-			var height      = rectTransform.rect.height;
-			var upperPivotY = (1 - rectTransform.pivot.y);
+        public static float GetLeftWidth(this RectTransform rectTransform)
+        {
+            var width = rectTransform.rect.width;
+            var leftPivotX = rectTransform.pivot.x;
 
-			var upperHeight = height * upperPivotY;
-			return upperHeight;
-		}
+            var leftWidth = width * leftPivotX;
+            return leftWidth;
+        }
 
-		public static float GetLowerHeight(this RectTransform rectTransform)
-		{
-			var height      = rectTransform.rect.height;
-			var lowerPivotY = rectTransform.pivot.y;
+        public static float GetRightWidth(this RectTransform rectTransform)
+        {
+            var width = rectTransform.rect.width;
+            var rightPivotX = (1 - rectTransform.pivot.x);
 
-			var lowerHeight = height * lowerPivotY;
-			return lowerHeight;
-		}
-	}
+            var rightWidth = width * rightPivotX;
+            return rightWidth;
+        }
+
+        public static float GetUpperHeight(this RectTransform rectTransform)
+        {
+            var height = rectTransform.rect.height;
+            var upperPivotY = (1 - rectTransform.pivot.y);
+
+            var upperHeight = height * upperPivotY;
+            return upperHeight;
+        }
+
+        public static float GetLowerHeight(this RectTransform rectTransform)
+        {
+            var height = rectTransform.rect.height;
+            var lowerPivotY = rectTransform.pivot.y;
+
+            var lowerHeight = height * lowerPivotY;
+            return lowerHeight;
+        }
+    }
 }
