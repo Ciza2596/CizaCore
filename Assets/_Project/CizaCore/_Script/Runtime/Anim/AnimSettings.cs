7d16e1b32aa88a13e913a5ac175601efa276e664
Ciza2596 <Ciza2596@gmail.com>
2023-12-21 21:42:34 +0800

    0.0.73

--- Assets/_Project/CizaCore/_Script/Runtime/Anim/AnimSettings.cs
+++ Assets/_Project/CizaCore/_Script/Runtime/Anim/AnimSettings.cs
@@ -5,47 +5,53 @@ using UnityEngine;
 
 namespace CizaCore
 {
-	[Serializable]
-	public class AnimSettings
-	{
-		[SerializeField]
-		private string _showStateName = "Show";
+    [Serializable]
+    public class AnimSettings
+    {
+        [SerializeField]
+        private string _showStateName = "Show";
 
-		[SerializeField]
-		private float _showEndNormalizedTime = 0.95f;
+        [SerializeField]
+        private float _showEndNormalizedTime = 0.95f;
 
-		[Space]
-		[SerializeField]
-		private string _hideStateName = "Hide";
+        [Space]
+        [SerializeField]
+        private string _hideStateName = "Hide";
 
-		[SerializeField]
-		private float _hideEndNormalizedTime = 0.95f;
+        [SerializeField]
+        private float _hideEndNormalizedTime = 0.95f;
 
-		[Space]
-		[SerializeField]
-		private Animator _animator;
+        [Space]
+        [SerializeField]
+        private Animator _animator;
 
-		public bool IsActiveAndEnabled => _animator.isActiveAndEnabled;
+        public bool IsActiveAndEnabled => _animator.isActiveAndEnabled;
 
-		public void SetIsActive(bool isActive) =>
-			_animator.gameObject.SetActive(isActive);
+        public void SetIsActive(bool isActive) =>
+            _animator.gameObject.SetActive(isActive);
 
-		public void Refresh() =>
-			_animator.Refresh();
+        public void Refresh() =>
+            _animator.Refresh();
 
-		public void PlayShowStartAndPause() =>
-			_animator.PlayAtStartAndPause(Animator.StringToHash(_showStateName));
+        public void PlayShowStartAndPause() =>
+            _animator.PlayAtStartAndPause(Animator.StringToHash(_showStateName));
 
-		public UniTask PlayShowAsync(CancellationToken cancellationToken) =>
-			PlayShowAsync(0, _showEndNormalizedTime, cancellationToken);
+        public UniTask PlayShowAsync(CancellationToken cancellationToken) =>
+            PlayShowAsync(0, _showEndNormalizedTime, cancellationToken);
 
-		public async void PlayShowComplete() =>
-			await PlayShowAsync(1, 1, default);
+        public async void PlayShowComplete() =>
+            await PlayShowAsync(1, 1, default);
 
-		public UniTask PlayHideAsync(CancellationToken cancellationToken) =>
-			_animator.PlayAtStartAsync(Animator.StringToHash(_hideStateName), endNormalizedTime: _hideEndNormalizedTime, isContinue: true, cancellationToken: cancellationToken);
+        public UniTask PlayHideAsync(CancellationToken cancellationToken) =>
+            PlayHideAsync(0, _hideEndNormalizedTime, cancellationToken);
 
-		private UniTask PlayShowAsync(float startNormalizedTime, float endNormalizedTime, CancellationToken cancellationToken) =>
-			_animator.PlayAsync(Animator.StringToHash(_showStateName), startNormalizedTime: startNormalizedTime, endNormalizedTime: endNormalizedTime, isContinue: true, cancellationToken: cancellationToken);
-	}
+        public async void PlayHideComplete() =>
+            await PlayHideAsync(1, 1, default);
+
+        private UniTask PlayShowAsync(float startNormalizedTime, float endNormalizedTime, CancellationToken cancellationToken) =>
+            _animator.PlayAsync(Animator.StringToHash(_showStateName), startNormalizedTime: startNormalizedTime, endNormalizedTime: endNormalizedTime, isContinue: true, cancellationToken: cancellationToken);
+
+        private UniTask PlayHideAsync(float startNormalizedTime, float endNormalizedTime, CancellationToken cancellationToken) =>
+            _animator.PlayAsync(Animator.StringToHash(_hideStateName), startNormalizedTime: startNormalizedTime, endNormalizedTime: endNormalizedTime, isContinue: true, cancellationToken: cancellationToken);
+    }
 }
