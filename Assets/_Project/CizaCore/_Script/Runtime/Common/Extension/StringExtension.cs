82731998d0fee1774369e613b6c517957a5e5ca0
Ciza2596 <Ciza2596@gmail.com>
2023-12-28 15:35:02 +0800

    0.0.80

--- Assets/_Project/CizaCore/_Script/Runtime/Common/Extension/StringExtension.cs
+++ Assets/_Project/CizaCore/_Script/Runtime/Common/Extension/StringExtension.cs
@@ -3,99 +3,114 @@ using System.Linq;
 
 namespace CizaCore
 {
-	public static class StringExtension
-	{
-		public const char SplitTag = ',';
+    public static class StringExtension
+    {
+        public const char SplitTag = ',';
 
-		public static bool IsContains(this string[] strs, string[] targetStrs)
-		{
-			foreach (var targetStr in targetStrs)
-			{
-				if (!strs.Contains(targetStr))
-					return false;
-			}
+        public static bool IsContains(this string[] strs, string[] targetStrs)
+        {
+            foreach (var targetStr in targetStrs)
+            {
+                if (!strs.Contains(targetStr))
+                    return false;
+            }
 
-			return true;
-		}
+            return true;
+        }
 
-		public static bool HasValue(this string str) =>
-			!string.IsNullOrEmpty(str) && !string.IsNullOrWhiteSpace(str);
+        public static bool HasValue(this string str) =>
+            !string.IsNullOrEmpty(str) && !string.IsNullOrWhiteSpace(str);
 
-		public static string WithoutSpace(this string str) =>
-			str.Replace(" ", "");
+        public static string WithoutSpace(this string str) =>
+            str.Replace(" ", "");
 
-		public static string[] ToArray(this string str, bool isIgnoreEmpty = true) =>
-			str.ToList(isIgnoreEmpty).ToArray();
+        public static string[] ToArray(this string str, bool isIgnoreEmpty = true) =>
+            str.ToList(isIgnoreEmpty).ToArray();
 
-		public static List<string> ToList(this string str, bool isIgnoreEmpty = true)
-		{
-			var list = new List<string>();
-			if (!str.HasValue())
-				return list;
+        public static List<string> ToList(this string str, bool isIgnoreEmpty = true)
+        {
+            var list = new List<string>();
+            if (!str.HasValue())
+                return list;
 
-			var strWithoutSpace = str.WithoutSpace();
-			if (!strWithoutSpace.Contains(SplitTag))
-			{
-				list.Add(strWithoutSpace);
-				return list;
-			}
+            var strWithoutSpace = str.WithoutSpace();
+            if (!strWithoutSpace.Contains(SplitTag))
+            {
+                list.Add(strWithoutSpace);
+                return list;
+            }
 
-			var splitStrs = strWithoutSpace.Split(SplitTag);
-			for (var i = 0; i < splitStrs.Length; i++)
-			{
-				var splitStr = splitStrs[i];
-				if (!splitStr.HasValue())
-				{
-					if (!isIgnoreEmpty && i != splitStrs.Length - 1)
-						list.AddEmptyItem(1);
-					continue;
-				}
+            var splitStrs = strWithoutSpace.Split(SplitTag);
+            for (var i = 0; i < splitStrs.Length; i++)
+            {
+                var splitStr = splitStrs[i];
+                if (!splitStr.HasValue())
+                {
+                    if (!isIgnoreEmpty && i != splitStrs.Length - 1)
+                        list.AddEmptyItem(1);
+                    continue;
+                }
 
-				list.Add(splitStr);
-			}
+                list.Add(splitStr);
+            }
 
-			return list;
-		}
+            return list;
+        }
 
-		public static string[] ToArray(this string str, int length, bool isIgnoreEmpty = true) =>
-			str.ToList(length, isIgnoreEmpty).ToArray();
+        public static string[] ToArray(this string str, int length, bool isIgnoreEmpty = true) =>
+            str.ToList(length, isIgnoreEmpty).ToArray();
 
-		public static List<string> ToList(this string str, int count, bool isIgnoreEmpty = true)
-		{
-			var list = new List<string>();
-			if (!str.HasValue())
-				return list.AddEmptyItem(count);
+        public static List<string> ToList(this string str, int count, bool isIgnoreEmpty = true)
+        {
+            var list = new List<string>();
+            if (!str.HasValue())
+                return list.AddEmptyItem(count);
 
-			var strWithoutSpace = str.WithoutSpace();
-			if (!strWithoutSpace.Contains(SplitTag))
-			{
-				list.Add(strWithoutSpace);
-				return list.AddEmptyItem(count - 1);
-			}
+            var strWithoutSpace = str.WithoutSpace();
+            if (!strWithoutSpace.Contains(SplitTag))
+            {
+                list.Add(strWithoutSpace);
+                return list.AddEmptyItem(count - 1);
+            }
 
-			var splitStrs = strWithoutSpace.Split(SplitTag);
-			for (var i = 0; i < splitStrs.Length; i++)
-			{
-				var splitStr = splitStrs[i];
-				if (!splitStr.HasValue())
-				{
-					if (!isIgnoreEmpty && i != splitStrs.Length - 1)
-						list.AddEmptyItem(1);
-					continue;
-				}
+            var splitStrs = strWithoutSpace.Split(SplitTag);
+            for (var i = 0; i < splitStrs.Length; i++)
+            {
+                var splitStr = splitStrs[i];
+                if (!splitStr.HasValue())
+                {
+                    if (!isIgnoreEmpty && i != splitStrs.Length - 1)
+                        list.AddEmptyItem(1);
+                    continue;
+                }
 
-				list.Add(splitStr);
-			}
+                list.Add(splitStr);
+            }
 
-			return list.AddEmptyItem(count - list.Count);
-		}
+            return list.AddEmptyItem(count - list.Count);
+        }
 
-		public static List<string> AddEmptyItem(this List<string> strings, int count)
-		{
-			for (int i = 0; i < count; i++)
-				strings.Add(string.Empty);
+        public static List<string> AddEmptyItem(this List<string> strings, int count)
+        {
+            for (int i = 0; i < count; i++)
+                strings.Add(string.Empty);
 
-			return strings;
-		}
-	}
+            return strings;
+        }
+
+        public static bool TryGetIndex(this string[] strs, string checkedStr, out int index)
+        {
+            for (var i = 0; i < strs.Length; i++)
+            {
+                if (strs[i] == checkedStr)
+                {
+                    index = i;
+                    return true;
+                }
+            }
+
+            index = 0;
+            return false;
+        }
+    }
 }
