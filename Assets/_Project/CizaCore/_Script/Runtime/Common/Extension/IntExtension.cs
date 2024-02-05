d056f7ad63a67e1330570caf7dd739a411776659
Ciza2596 <Ciza2596@gmail.com>
2024-02-05 18:45:08 +0800

    Update 0.1.6

--- Assets/_Project/CizaCore/_Script/Runtime/Common/Extension/IntExtension.cs
+++ Assets/_Project/CizaCore/_Script/Runtime/Common/Extension/IntExtension.cs
@@ -6,11 +6,14 @@ namespace CizaCore
     {
         // Odd: 0, Even: 1
         public static int ToOddOrEvenIndex(this int index) =>
-            (index + 1) % 2 == 1 ? 0 : 1;
+            CheckIsOdd(index + 1) ? 0 : 1;
 
         // Odd: 1, Even: 2
         public static int ToOddOrEven(this int value) =>
-            value % 2 == 1 ? 1 : 2;
+            CheckIsOdd(value) ? 1 : 2;
+
+        public static bool CheckIsOdd(this int value) =>
+            value % 2 == 1;
 
 
         public static int ToClamp01(this int value, bool isCircle = false) =>
