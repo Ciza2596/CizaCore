809aeb5daee6f36ab282a79479d37fc25ae83870
Ciza Huang <ciza2596@gmail.com>
2025-12-07 11:34:30 +0800

    Add CizaUniTask

--- Assets/_Project/CizaCore/_Script/Runtime/Anim/AnimMonoPlayer/AnimMonoPlayer.cs
+++ Assets/_Project/CizaCore/_Script/Runtime/Anim/AnimMonoPlayer/AnimMonoPlayer.cs
@@ -1,4 +1,5 @@
-using Cysharp.Threading.Tasks;
+
+using CizaUniTask;
 using UnityEngine;
 
 namespace CizaCore
--- Assets/_Project/CizaCore/_Script/Runtime/Anim/AnimSettings.cs
+++ Assets/_Project/CizaCore/_Script/Runtime/Anim/AnimSettings.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Threading;
-using Cysharp.Threading.Tasks;
+using CizaUniTask;
 using UnityEngine;
 
 namespace CizaCore
